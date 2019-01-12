3f3aaea49ad1c33ed444918808fc1aee9a22f526
Eric Ampire <ericampire.top@gmail.com>
2019-01-12 10:38:10 +0200

    Adding viewpager

--- app/src/main/java/club/pbreakers/training/MainActivity.kt
+++ app/src/main/java/club/pbreakers/training/MainActivity.kt
@@ -1,12 +1,24 @@
 package club.pbreakers.training
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnViewPager.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btnViewPager -> startActivity(Intent(this, ViewPagerActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/club/pbreakers/training/ViewPagerActivity.kt
@@ -0,0 +1,12 @@
+package club.pbreakers.training
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPagerActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_pager)
+    }
+}
