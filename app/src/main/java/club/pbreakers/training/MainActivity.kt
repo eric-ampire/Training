306243f32192ac597434399e6980f4e4b8f17d7a
Eric Ampire <ericampire.top@gmail.com>
2019-01-12 10:44:26 +0200

    Migration vers AndroidX

--- app/src/main/java/club/pbreakers/training/MainActivity.kt
+++ app/src/main/java/club/pbreakers/training/MainActivity.kt
@@ -1,7 +1,7 @@
 package club.pbreakers.training
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
--- app/src/main/java/club/pbreakers/training/ViewPagerActivity.kt
+++ app/src/main/java/club/pbreakers/training/ViewPagerActivity.kt
@@ -1,6 +1,6 @@
 package club.pbreakers.training
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class ViewPagerActivity : AppCompatActivity() {
