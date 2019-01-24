162fc3f851bfff966a7f04c965146a63da69f5f5
Eric Ampire <ericampire.top@gmail.com>
2019-01-24 09:44:35 +0200

    Adding Entity.kt

--- app/src/main/java/club/pbreakers/training/database/Entity.kt
+++ app/src/main/java/club/pbreakers/training/database/Entity.kt
@@ -1,2 +1,26 @@
 package club.pbreakers.training.database
 
+import androidx.room.*
+
+@Entity(tableName = "parent")
+data class Parent(
+    @PrimaryKey val id: Int,
+    val nom: String,
+    val postNom: String
+)
+
+@Entity(tableName = "enfant")
+data class Enfant(
+    @PrimaryKey val id: Int,
+    val idParent: Int,
+    val idEcole: Int,
+    val nom: String,
+    val postnom: String
+)
+
+@Entity(tableName = "ecole")
+data class Ecole(
+    @PrimaryKey val id: Int,
+    val nom: String,
+    val adresse: String
+)
