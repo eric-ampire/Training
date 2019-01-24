82b28b91163071c32fb0c036a848bc990ad4ff6f
Eric Ampire <ericampire.top@gmail.com>
2019-01-24 09:43:58 +0200

    Update AppDatabase.kt

--- app/src/main/java/club/pbreakers/training/database/AppDatabase.kt
+++ app/src/main/java/club/pbreakers/training/database/AppDatabase.kt
@@ -1,2 +1,11 @@
 package club.pbreakers.training.database
 
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(version = 1, entities = [Parent::class, Enfant::class, Ecole::class])
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun getParentDAO() : ParentDAO
+    abstract fun getEcoleDAO() : EcoleDAO
+    abstract fun getEnfant() : EnfantDAO
+}
