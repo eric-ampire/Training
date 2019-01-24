2689cdf4bd5e06b20089af2bde10c17a4bcef421
Eric Ampire <ericampire.top@gmail.com>
2019-01-24 09:45:04 +0200

    Update EcoleoActivity.kt

--- app/src/main/java/club/pbreakers/training/room/EcoleActivity.kt
+++ app/src/main/java/club/pbreakers/training/room/EcoleActivity.kt
@@ -1,12 +1,21 @@
 package club.pbreakers.training.room
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.loader.content.AsyncTaskLoader
+import club.pbreakers.training.R
+import club.pbreakers.training.database.Ecole
 
 class EcoleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_ecole)
+    }
+
+    fun showAddEcoleDialog(view: View) {
+
     }
 }
