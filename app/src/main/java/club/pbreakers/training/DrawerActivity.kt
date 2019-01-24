f009deeede642e9b0e29c778b314238fd60fe0d0
Eric Ampire <ericampire.top@gmail.com>
2019-01-24 09:46:04 +0200

    Adding DrawerActivity.kt

--- app/src/main/java/club/pbreakers/training/DrawerActivity.kt
+++ app/src/main/java/club/pbreakers/training/DrawerActivity.kt
@@ -2,11 +2,44 @@ package club.pbreakers.training
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_drawer.*
 
-class DrawerActivity : AppCompatActivity() {
+class DrawerActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onNavigationItemSelected(menu: MenuItem): Boolean {
+        return when (menu.itemId) {
+            R.id.about_menu -> {
+                drawer.closeDrawer(GravityCompat.START)
+                true
+            }
+
+            R.id.home_menu -> {
+                drawer.closeDrawer(GravityCompat.START)
+                true
+            }
+
+            R.id.setting_menu -> {
+                drawer.closeDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> false
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drawer)
+
+        setSupportActionBar(toolbar)
+
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.app_name, R.string.app_name)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setNavigationItemSelectedListener(this)
     }
 }
