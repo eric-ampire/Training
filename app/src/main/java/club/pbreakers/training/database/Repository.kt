f804d2f1bdfe1d086be10997f7bf5683aa97bc6a
Eric Ampire <ericampire.top@gmail.com>
2019-01-25 16:17:10 +0200

    Adding Repository.kt

--- app/src/main/java/club/pbreakers/training/database/Repository.kt
+++ app/src/main/java/club/pbreakers/training/database/Repository.kt
@@ -1,2 +1,15 @@
 package club.pbreakers.training.database
 
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import kotlin.concurrent.thread
+
+class ParentRepo(val ctx: Context, val db: RoomDatabase) {
+    fun addParent(parent: Parent) {
+        Room.databaseBuilder(ctx, AppDatabase::class.java, "training-db").build()
+        thread {
+
+        }
+    }
+}
